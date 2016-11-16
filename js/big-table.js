f59c73cfc89e47a37c1dde3baf62c9ad85aac44e
Anastasiya Lisovenko <lisovenko.ad@gmail.com>
2016-11-16 11:53:15 +0300

    Tune big table view

--- js/big-table.js
+++ js/big-table.js
@@ -8,36 +8,38 @@ bigTable = new webix.ui({
         view:"datatable",
         hover:"cell-hover",
         columns:[
-            { id:"NumbCat",   header:[{ content:"textFilter", placeholder: "№ по каталогу", height: "68" }],  sort:"int", width:80},
-            { id:"Title",   header:[{ content:"textFilter", placeholder: "Название", height: "68" }], sort:"string", fillspace: true},
-            { id:"ArchNumb", header:[{ content:"textFilter", placeholder: "Шифр", height: "68" }], sort:"string", fillspace: true},
-            { id:"Size", header:[{ content:"textFilter", placeholder: "Размер", height: "68" }], sort:"string", width: 105 },
-            { id:"Technique", header:[{ content:"textFilter", placeholder: "Техника", height: "68" }], sort:"string", width: 108 },
-            { id:"Watermarks", header:[{ content:"textFilter", placeholder: "Знаки", height: "68" }], sort:"string", fillspace: true},
-            { id:"Comment", header:[{ content:"textFilter", placeholder: "Комментарии", height: "68" }], sort:"string", fillspace: true},
-            { id:"DateLow", header:[{ content:"textFilter", placeholder: "От", height: "68" }], sort:"string", width: 90 },
-            { id:"DateUpp", header:[{ content:"textFilter", placeholder: "До", height: "68" }], sort:"string", width: 90 },
-            { id:"Symbols", header:[{ content:"textFilter", placeholder: "Символы", height: "68" }], sort:"string", fillspace: true},
-            { id:"Text", header:[{ content:"textFilter", placeholder: "Текст", height: "68" }], sort:"string", fillspace: true},
-            { id:"TextRev", header:[{ content:"textFilter", placeholder: "Текст обр.", height: "68" }], sort:"string", fillspace: true},
-            { id:"Bibliogr", header:[{ content:"textFilter", placeholder: "Библ.", height: "68" }], sort:"string", fillspace: true},
-            { id:"Details", header:[{ content:"textFilter", placeholder: "Детали", height: "68" }], sort:"string", fillspace: true}
+            { id:"NumbCat",   header:[{ content:"textFilter", placeholder: "№", height: "46" }],  sort:"int", width: 40},
+            { id:"Title",   header:[{ content:"textFilter", placeholder: "Название", height: "46" }], sort:"string", fillspace: 2, minWidth: 150},
+            { id:"ArchNumb", header:[{ content:"textFilter", placeholder: "Шифр", height: "46" }], sort:"string", fillspace: 1.5, minWidth: 100},
+            { id:"Size", header:[{ content:"textFilter", placeholder: "Размер", height: "46" }], sort:"string", fillspace: true, minWidth: 80},
+            { id:"Technique", header:[{ content:"textFilter", placeholder: "Техника", height: "46" }], sort:"string", fillspace: true, minWidth: 80},
+            { id:"Watermarks", header:[{ content:"textFilter", placeholder: "Знаки", height: "46" }], sort:"string", fillspace: true, minWidth: 80},
+            { id:"Comment", header:[{ content:"textFilter", placeholder: "Комментарии", height: "46" }], sort:"string", fillspace: true, minWidth: 80},
+            { id:"DateLow", header:[{ content:"textFilter", placeholder: "От", height: "46" }], sort:"string", width: 40 },
+            { id:"DateUpp", header:[{ content:"textFilter", placeholder: "До", height: "46" }], sort:"string", width: 40},
+            { id:"Symbols", header:[{ content:"textFilter", placeholder: "Символы", height: "46" }], sort:"string", fillspace: 1.5, minWidth: 150},
+            { id:"Text", header:[{ content:"textFilter", placeholder: "Текст", height: "46" }], sort:"string", fillspace: 5, minWidth: 400},
+            { id:"TextRev", header:[{ content:"textFilter", placeholder: "Текст обр.", height: "46" }], sort:"string", fillspace: 2, minWidth: 200},
+            { id:"Bibliogr", header:[{ content:"textFilter", placeholder: "Библ.", height: "46" }], sort:"string", fillspace: true, minWidth: 80},
+            { id:"Details", header:[{ content:"textFilter", placeholder: "Детали", height: "46" }], sort:"string", fillspace: true, minWidth: 80}
 
         ],
         select:"row",
         fixedRowHeight:false,
         rowLineHeight:25,
         rowHeight:25,
-        scrollX:false,
+        scrollX:true,
+        resizeRow:true,
+        css: "table--s",
         on: {
             onAfterLoad: function () {
                 webix.delay(function () {
-                    this.adjustRowHeight("Title", true);
+                    this.adjustRowHeight("Text", true);
                     this.render();
                 }, this);
             },
             onColumnResize: function () {
-                this.adjustRowHeight("Title", true);
+                this.adjustRowHeight("Text", true);
                 this.render();
             },
             onItemClick : function() {
